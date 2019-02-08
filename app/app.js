91e533d42562c8a54e4539eb84a130ad0a9fe661
ivardesign <m.ivar.johnson@gmail.com>
2019-02-08 10:51:15 -0800

    added some elements to index, notes to app and basic outline for style

--- app/app.js
+++ app/app.js
@@ -1,3 +1,26 @@
 // init app
 // interact with DOM
 // interact with localstorage
+/*
+window.localStorage (mdn)
+
+localStorage.setItem('hrext07', 'r00lz');
+
+var localTest = localStorage.getItem('hrext07');
+
+localStorage.removeItem('hrext07');
+
+localStorage.clear(); //clears all local storage
+
+*/
+$(document).ready(function() {
+  // write to db
+
+  // read from db
+
+  // update db
+
+  // delete item
+
+  // delete (all?)
+});
