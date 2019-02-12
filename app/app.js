cb43f6c6e1dabe7558e44ea940b4f37cf3e995cb
ivardesign <m.ivar.johnson@gmail.com>
2019-02-12 13:12:56 -0800

    Implementation planning begun.  Reconsidering project (seems data manipulation may be too minimal)

--- app/app.js
+++ app/app.js
@@ -1,3 +1,42 @@
+/* ----------------------------------------------------------------------------------- */
+    // ---------- brainstorm ----------
+    // clear inputs on button click (add button)
+    // every entry should be appened as a new .display-entry item (switch to append)
+    // every entry should have it's own edit and delete.
+    // duplicate keys (add button) should not be permitted >> maybe do away with manually entered keys - replace with incrementing or timestamp, don't display key id's in output but tie edit/delete to id for each
+    //-- make list sortable >> drag-n-drop
+    // edit function needs work too
+    // make UI pretty and aria compliant
+
+    // ---------- Order ideas ----------
+    // clear inputs on click of 'add' button
+    // make 'add' generate unique key (id) for each entry and get rid of the 'key' input
+    // 'add' function should include
+        // entry
+        // edit button
+        // delete button
+    // re-arrange the UI
+    // 'clear' should be obvious, near ouput (away from form) and say 'clear all'
+
+    // ---------- Implementation ----------
+
+    // >>> clear inputs function plan <<<
+    // tied to 'add' click-event (what about when someone just hits return/enter?)
+    // search MDN : clear input on click
+
+    // >>> Add generates unique key (id) function plan <<<
+    // John mentioned something about uuid generater ... see if that's something to use
+    // mdn uuid and search uuid generator
+
+    // once set up, remove add input from UI
+    // Add must also include Edit and Delete, tied to key of entry
+    // Add must 'append' new entries (replace '.html')
+    // do buttons need to be in forms? what happens to listeners when we append?
+
+
+/* ----------------------------------------------------------------------------------- */
+
+
 // init app
 // interact with DOM
 // interact with localstorage
@@ -25,25 +64,6 @@ $(document).ready(function() {
     // and append a div
     // <div class="display-data-item" data-keyValue="keyData">valueData</div>
     //ToDo make these vars make sense accross the app
-/* ----------------------------------------------------------------------------------- */
-    // ---------- brainstorm ----------
-    // clear inputs on button click (add button)
-    // every entry should be appened as a new .display-entry item (switch to append)
-    // every entry should have it's own edit and delete.
-    // duplicate keys (add button) should not be permitted >> maybe do away with manually entered keys - replace with incrementing or timestamp, don't display key id's in output but tie edit/delete to id for each
-    //-- make list sortable >> drag-n-drop
-    // edit function needs work too
-    // make UI pretty and aria compliant
-
-    // ---------- Order ideas ----------
-    // clear inputs on click of 'add' button
-    // make 'add' generate unique key (id) for each entry and get rid of the 'key' input
-    // 'add' function should include
-        // entry
-        // edit button
-        // delete button
-    // re-arrange the UI
-    // 'clear' should be obvious, near ouput (away from form) and say 'clear all'
 
     $('.display-entry').append(`<div class="display-data-item ` + keyData + `" data-keyValue="keyData">` + valueData + `</div>`);
   });
