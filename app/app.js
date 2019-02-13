c0cdd1775fc4af64089d63b6c35e072b99e9aa8f
ivardesign <m.ivar.johnson@gmail.com>
2019-02-12 17:26:27 -0800

    house-cleaning on app.js

--- app/app.js
+++ app/app.js
@@ -19,48 +19,22 @@
     // 'clear' should be obvious, near ouput (away from form) and say 'clear all'
 
     // ---------- Implementation ----------
-
     // >>> clear inputs function plan <<<
     // tied to 'add' click-event (what about when someone just hits return/enter?)
-    // search MDN : clear input on click >>>>>>> try #addText .value = '';
     // >>> Add generates unique key (id) function plan <<<
-    // John mentioned something about uuid generater ... see if that's something to use
-    // mdn uuid and search uuid generator
     // once set up, remove add input from UI
     // Add must also include Edit and Delete, tied to key of entry
     // Add must 'append' new entries (replace '.html')
-    // do buttons need to be in forms? what happens to listeners when we append?
-/* ----------------------------------------------------------------------------------- */
-// init app
-// interact with DOM
-// interact with localstorage
-/*
-window.localStorage (mdn)
-localStorage.setItem('hrext07', 'r00lz');
-var localTest = localStorage.getItem('hrext07');
-localStorage.removeItem('hrext07');
-localStorage.clear(); //clears all local storage
 
-// uuid generator
-// db could be a object to expand more than 1 item is added
-*/
+/* ----------------------------------------------------------------------------------- */
 $(document).ready(function() {
   $('.submit').click(function(e) {
-    //console.log(e);
-    var keyData = Date.now();// use instead of asking for key entry.
+    var keyData = Date.now();// use timestamp instead of asking for a key entry.
     var valueData = $('#addText').val();
-    //$('#input-key').val('');// clear input
     $('#addText').val(''); // clear input
     // write to db
     localStorage.setItem(keyData, valueData);
-    var displayText = keyData + ' | ' + localStorage.getItem(keyData);
-    //console.log(localStorage.getItem(keyData, valueData));
-
-    // read from db
-    // and append a div
-    // <div class="display-data-item" data-keyValue="keyData">valueData</div>
-    //ToDo make these vars make sense accross the app
-
+    // write to display
     $('.todos').append(`<div class="display-data-item ` +
                          keyData +
                          `" data-keyValue="keyData"><span class="item">` +
@@ -86,7 +60,7 @@ $(document).ready(function() {
   });
 
   // clear all
-  $('.clear').click(function() {// clear all .todos
+  $('.clear').click(function() {
     localStorage.clear();// clears localstorage
     $('.todos').html('');// clear the .todos box
   });
